00afe1c1a96ce2db846b0c3972182b61acc0b4c2
Antonio Quiles <antonioquiles.dev@gmail.com>
2026-05-28 14:09:33 +0200

    fix: replace process.env with static values in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  supabaseUrl: process.env['SUPABASE_URL'] ?? '',
-  supabaseAnonKey: process.env['SUPABASE_ANON_KEY'] ?? '',
-  apiUrl: process.env['API_URL'] ?? '',
+  supabaseUrl: 'https://muwnbgnatvjjzlhnkjsq.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11d25iZ25hdHZqanpsaG5ranNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDc3MjMsImV4cCI6MjA5NTQyMzcyM30.-lhJ5tIAkGyfRxhQklnWieUGcqdYb3QW-genILQ-UYo',
+  apiUrl: 'https://price-alert-kohl.vercel.app',
 };
